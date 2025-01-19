2af13ed039427b8573f8565b60fd538ae6756a6e
Leo Bi <biminglei@gmail.com>
2025-01-19 10:44:31 +0800

    add utils

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
+from utils import create_text_image
+
 st.title("Best Wishes 🌸")
 
-st.write("DB Name: " + st.secrets["db_name"])
-st.write("DB Name2: " + st.secrets["db_name"])
+initial_text = """明日朝阳映瑞雪，磊落人生喜气多。祝愿声声传四海，您心所愿皆成真。新春佳节福星照，春风送暖入屠苏。快意人生添欢笑，乐享天伦共此时。"""
+
+para = st.text_area("输入一段话吧?", value=initial_text )
+
+st.write(para)
+
+# 创建一个按钮
+if st.button("生成图片"):
+    # 当按钮被点击时，生成图片
+    image = create_text_image(my_text = para)
+    # 显示生成的图片
+    st.image(image, caption="生成的图片")
+
