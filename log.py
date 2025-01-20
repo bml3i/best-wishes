52987d4b9f3a6a057eda235566a86cf20501ce55
Leo Bi <biminglei@gmail.com>
2025-01-20 22:16:17 +0800

    update logger

--- log.py
+++ log.py
@@ -1,68 +1,37 @@
-import logging
-
 import streamlit as st
-from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
-
-
-logger = logging.getLogger(st.__name__)
-logger.setLevel(logging.INFO)
-
-st.title("测试日志")
-
-logger.info("This app started !!!")
+import logging
+import requests
 
 def get_remote_ip() -> str:
-    """Get remote ip."""
-
-    try:
-        ctx = get_script_run_ctx()
-        if ctx is None:
-            return None
-
-        session_info = runtime.get_instance().get_client(ctx.session_id)
-        if session_info is None:
-            return None
-    except Exception as e:
-        return None
-
-    return session_info.request.remote_ip
+    if "my_ip" not in st.session_state:
+        data = requests.get("https://api-bdc.net/data/client-ip").json()
+        st.session_state["my_ip"] = data["ipString"]
+        return st.session_state["my_ip"]
+    else:
+        return st.session_state["my_ip"]
 
 class ContextFilter(logging.Filter):
     def filter(self, record):
         record.user_ip = get_remote_ip()
         return super().filter(record)
 
-def init_logging():
-    # Make sure to instanciate the logger only once
-    # otherwise, it will create a StreamHandler at every run
-    # and duplicate the messages
-
-    # create a custom logger
-    logger = logging.getLogger("foobar")
-    if logger.handlers:  # logger is already setup, don't setup again
-        return
+logger = logging.getLogger(st.__name__)
+if not logger.handlers:
     logger.propagate = False
     logger.setLevel(logging.INFO)
-    # in the formatter, use the variable "user_ip"
-    formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s [user_ip=%(user_ip)s] - %(message)s")
+    formatter = logging.Formatter("%(asctime)s %(levelname)s [%(user_ip)s] - %(message)s")
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
     handler.addFilter(ContextFilter())
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-def main():
-    logger.info("Inside main")
-    st.title("Title")
+st.title("Logger Testing")
 
-    text = st.sidebar.text_input("Text:")
-    logger.info(f"This is the text: {text}")
+logger.info("Inside main")
 
+text = st.sidebar.text_input("Text:")
+logger.info(f"This is the text: {text}")
 
-if __name__ == "__main__":
-    init_logging()
-    logger = logging.getLogger("foobar")
-    main()
-
-
+data = requests.get("https://api-bdc.net/data/client-ip").json()
+st.write(data["ipString"])
