2c9471d6beb8a75786398bd595eca36a759d0ee2
Leo Bi <biminglei@gmail.com>
2025-01-20 21:34:36 +0800

    test logger

--- log.py
+++ log.py
@@ -1,11 +1,68 @@
-import streamlit as st
 import logging
 
-logger = logging.getLogger("simple_logger")
+import streamlit as st
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+logger = logging.getLogger(st.__name__)
+logger.setLevel(logging.INFO)
 
 st.title("测试日志")
 
-if st.button("点击开始记录日志"):
-    print("01 - This is a print info - B01")
-    logger.info("02 - Still running task .")
-    logger.info("03 - Still running task ... ")
+logger.info("This app started !!!")
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        record.user_ip = get_remote_ip()
+        return super().filter(record)
+
+def init_logging():
+    # Make sure to instanciate the logger only once
+    # otherwise, it will create a StreamHandler at every run
+    # and duplicate the messages
+
+    # create a custom logger
+    logger = logging.getLogger("foobar")
+    if logger.handlers:  # logger is already setup, don't setup again
+        return
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    # in the formatter, use the variable "user_ip"
+    formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s [user_ip=%(user_ip)s] - %(message)s")
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.addFilter(ContextFilter())
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+def main():
+    logger.info("Inside main")
+    st.title("Title")
+
+    text = st.sidebar.text_input("Text:")
+    logger.info(f"This is the text: {text}")
+
+
+if __name__ == "__main__":
+    init_logging()
+    logger = logging.getLogger("foobar")
+    main()
+
+
